9b14b2ff37b9247f88195b364cbe3f41d47aa458
inesjou <inesjoumaili@gmaiL.com>
2023-12-18 11:15:40 +0100

    Styled the icon (size and position)

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ function refreshWeather(response) {
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
-  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" width=60px/>`;
 }
 
 function formatDate(date) {
