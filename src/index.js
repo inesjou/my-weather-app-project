8d6df66240750a4d94539cdae771b56a0c2f7e5e
inesjou <inesjoumaili@gmaiL.com>
2023-12-18 10:43:52 +0100

    Added API with current temperature

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 function citySearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#form-input");
-  let searchedCity = document.querySelector("#searched-city");
-  searchedCity.innerHTML = searchInput.value;
+
+  apiSearch(searchInput.value);
+}
+
+function apiSearch(city) {
+  let apiKey = "43btb7f306f4a0569633eb0o88c9fec4";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let currentTemperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#searched-city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(currentTemperature);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", citySearch);
+
+apiSearch("Paris");
